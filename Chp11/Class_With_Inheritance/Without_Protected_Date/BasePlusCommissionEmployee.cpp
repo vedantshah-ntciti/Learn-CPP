810b013d4ec96f979bdf4fe9cd903d0187428e10
vedantshah-ntciti <vedantshah@iitbhilai.ac.in>
2026-08-21 16:42:41 +0530

    Inheritance without making proper base constuctor, will fail to compile

---
+++ Chp11/Class_With_Inheritance/Without_Protected_Date/BasePlusCommissionEmployee.cpp
@@ -0,0 +1,35 @@
+//Supposed to throw compiler as derived class cannot access base class private members
+#include <iostream>
+#include <stdexcept>
+#include "BasePlusCommissionEmployee.h"
+using namespace std;
+
+BasePlusCommissionEmployee::BasePlusCommissionEmployee(const string & first, const string & last, const string & pan, double sales, double rate , double salary) : CommissionEmployee(first, last, pan, sales, rate)
+{
+    setBaseSalary(salary);
+}
+
+void BasePlusCommissionEmployee::print() const
+{
+    cout << "commission employees: " << firstName << ' ' << lastName << "\nPAN: "  << PanNumber << "\ngross sales: " << grossSales << "\nCommissionRate: " << commissionRates << "\nBaseSalary: " << baseSalary << endl;
+} 
+
+double BasePlusCommissionEmployee::earnings() const
+{
+    return baseSalary + commissionRates*grossSales;
+} 
+
+double BasePlusCommissionEmployee::getBaseSalary() const
+{
+    return baseSalary;
+}
+
+void BasePlusCommissionEmployee::setBaseSalary(double salary)
+{
+    if (salary < 0)
+    {
+        throw invalid_argument("baseSalary cannot be negative");
+    }
+
+    baseSalary = salary;
+}
