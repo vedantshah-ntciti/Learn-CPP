55910fb2d1cbe2fee055f0ab640dba5175b24b94
vedantshah-ntciti <vedantshah@iitbhilai.ac.in>
2026-09-02 15:44:50 +0530

    Reading data from a file

---
+++ Chp14/ReadingDataFromSeqFile/Read_Sequential_File.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+void outputLine( int , const string &, double);
+
+int main()
+{
+    ifstream inClientFile( "clients.txt" , ios::in );
+
+    if ( !inClientFile )
+    {
+        cerr << "File could not opened" << endl;
+        exit( EXIT_FAILURE );
+    }
+
+    int account;
+    string name;
+    double balance;
+
+    while ( inClientFile >> account >> name >> balance )
+    {
+        outputLine(account, name, balance);
+    }
+}
+
+void outputLine( int account , const string & name , double balance )
+{
+    cout << left << setw(10) << account << setw(13) << name << setw(7) << setprecision(2) << right << balance << endl;
+}
