e159f7459b2cae361dbe923bcc8a1c72c957971b
vedantshah-ntciti <vedantshah@iitbhilai.ac.in>
2026-09-09 02:02:00 +0530

    Multiset Associative Containers

---
+++ Chp15/Associative_Containers/multiset_associative_container/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <array>
+#include <set>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+
+int main()
+{
+    const size_t SIZE = 10;
+    array <int , SIZE> a = { 7, 22, 9, 1, 18, 30, 100, 22, 85, 13 };
+    multiset<int, less<int> > intMultiset;
+    ostream_iterator <int> output(cout , " ");
+
+    cout << "There are currently " << intMultiset.count(15) << " values of 15 in the multiset\n";
+
+    intMultiset.insert(15);
+    intMultiset.insert(15);
+
+    cout << "After inserts, there are " << intMultiset.count(15) << " values of 15 in the multiset\n";
+
+    auto result = intMultiset.find(15);
+
+    if (result != intMultiset.end())
+    {
+        cout << "Found value 15\n";
+    }
+
+    result = intMultiset.find(20);
+    if (result == intMultiset.end())
+    {
+        cout << "Did not find 20\n";
+    }
+
+    intMultiset.insert(a.cbegin() , a.cend());
+    cout << "After insert, intMultiset contains: \n";
+    copy(intMultiset.begin() , intMultiset.end() , output);
+
+    cout << "\n\nLower bound of 22: " << *(intMultiset.lower_bound(22) ) ;
+    cout << "\nUpper bound of 22: " << *(intMultiset.upper_bound(22) ) ; 
+
+    auto p = intMultiset.equal_range( 22 );
+    cout << "\n\nequal_range of 22: " << "\n    Lower bound: " << *(p.first ) << "\n    Upper bound: " << *(p.second) << endl;
+}
